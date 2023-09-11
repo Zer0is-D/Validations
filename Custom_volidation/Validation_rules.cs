a2278cb296c1b609ca86eb3f9d688d37a84534bf
Admin <Admin@DESKTOP-055I5NA>
2023-09-12 00:28:11 +0300

    Добавьте файлы проекта.

---
+++ Custom_volidation/Validation_rules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Custom_volidation
+{
+    public class Validation_rules : ValidationRule
+    {
+        public int MinimumCharecters { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string charString = value as string;
+
+            if (charString.Length < MinimumCharecters)
+                return new ValidationResult(false, $"Используйте еще как минимум {MinimumCharecters - charString.Length} символа");
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ Validations/RegistrationVM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validations
+{
+    public class RegistrationVM : ObservableObject, IDataErrorInfo
+    {
+        public string Error { get { return null; } }
+        private string _username;
+
+        public Dictionary<string, string> ErrorCollection { get; private set; } = new Dictionary<string, string>();
+
+        public string this[string name]
+        {
+            get
+            {
+                string result = null;
+
+                switch (name)
+                {
+                    case "Username":
+                        if (string.IsNullOrWhiteSpace(Username))
+                            result = "Имя не может быть пустым";
+                        else if (Username.Length < 5)
+                            result = "Имя не может быть меньше 5 символов";
+                        break;
+                }
+
+                if (ErrorCollection.ContainsKey(name))
+                    ErrorCollection[name] = result;
+                else if (result != null)
+                    ErrorCollection.Add(name, result);
+
+                OnPropertyChanged("ErrorCollection");
+
+                return result;
+            }
+        }
+
+        public string Username
+        {
+            get => _username;
+            set => OnPropertyChanged(ref _username, value);
+        }
+    }
+}
